0d6803da7eb732ac9d6f90c96d96174fd844376a
TaherJalal <taherjalaljuma@gmail.com>
2023-03-11 21:15:33 -0800

    added image path to landing path api

--- pages/api/landingpage.ts
+++ pages/api/landingpage.ts
@@ -172,12 +172,12 @@ export default async function landingpage(
   res.json([
     {
       id: Math.floor(Math.random() * 10),
-      title: "Trending Movies's",
+      title: "Trending Movie's",
       movies: await trendingMovies(),
     },
     {
       id: Math.floor(Math.random() * 100),
-      title: "Top Rated Movies's Of All Time's",
+      title: "Top Rated Movie's Of All Time's",
       movies: await popularMoviesOfAllTime(),
     },
     {
@@ -198,12 +198,24 @@ async function trendingMovies() {
     "https://api.themoviedb.org/3/discover/movie?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
   );
 
+  data.results.map(
+    (movie: any) =>
+      (movie.poster_path =
+        "https://image.tmdb.org/t/p/original" + movie.poster_path)
+  );
+
   return data.results;
 }
 
 async function popularMoviesOfAllTime() {
   const { data } = await axios.get(
     "https://api.themoviedb.org/3/discover/movie?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1"
+  );
+
+  data.results.map(
+    (movie: any) =>
+      (movie.poster_path =
+        "https://image.tmdb.org/t/p/original" + movie.poster_path)
   );
 
   return data.results;
@@ -214,12 +226,24 @@ async function popularTvShows() {
     "https://api.themoviedb.org/3/tv/popular?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&page=1"
   );
 
+  data.results.map(
+    (movie: any) =>
+      (movie.poster_path =
+        "https://image.tmdb.org/t/p/original" + movie.poster_path)
+  );
+
   return data.results;
 }
 
 async function popularTvShowsOfAllTime() {
   const { data } = await axios.get(
     "https://api.themoviedb.org/3/tv/top_rated?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&page=1"
+  );
+
+  data.results.map(
+    (movie: any) =>
+      (movie.poster_path =
+        "https://image.tmdb.org/t/p/original" + movie.poster_path)
   );
 
   return data.results;
